0561d8a6e6367b328c535ef6a89f6371fea0de33
ybuyanova <yulia@freeportmetrics.com>
2017-12-29 16:30:55 +0100

    mvvm architecture

---
+++ app/src/main/java/com/example/ybuyanova/weatherapp/WeatherInfoViewModel.java
@@ -0,0 +1,87 @@
+package com.example.ybuyanova.weatherapp;
+
+
+import android.content.Context;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.BindingAdapter;
+import android.databinding.InverseBindingAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+
+import java.util.List;
+
+
+public class WeatherInfoViewModel extends BaseObservable {
+
+    private WeatherInfo weatherInfo;
+
+    private Context mContext;
+
+    public WeatherInfoViewModel(WeatherInfo weatherInfo, Context mContext) {
+        this.weatherInfo = weatherInfo;
+        this.mContext = mContext;
+    }
+
+
+    @Bindable
+    public String getCity() {
+        return weatherInfo.getCity();
+    }
+
+    public void setCity(String city) {
+        weatherInfo.setCity(city);
+    }
+
+    @Bindable
+    public WeatherInfo.Temp getTemp() {
+        return weatherInfo.getTemp();
+    }
+
+    public void setTemp(WeatherInfo.Temp temp) {
+
+        weatherInfo.setTemp(temp);
+    }
+
+    @Bindable
+    public WeatherInfo.WeatherDescription getDescription() {
+        return weatherInfo.getDescription().get(0);
+    }
+
+    public void setDescription(List<WeatherInfo.WeatherDescription> description) {
+        weatherInfo.setDescription(description);
+    }
+
+
+    @BindingAdapter("android:text")
+    public static void setText(TextView view, String value) {
+        view.setText(value);
+    }
+
+    @InverseBindingAdapter(attribute = "android:text")
+    public static String getText(TextView view) {
+        return view.getText().toString();
+    }
+
+    @BindingAdapter("android:text")
+    public static void setText(TextView view, double value) {
+        view.setText(Double.toString(value));
+    }
+
+    @InverseBindingAdapter(attribute = "android:text")
+    public static double getTextDouble(TextView view) {
+        return Double.parseDouble(view.getText().toString());
+    }
+
+    @BindingAdapter({"bind:icon"})
+    public static void loadImage(ImageView view, String imageUrl) {
+        Picasso.with(view.getContext())
+                .load(imageUrl)
+                .into(view);
+    }
+
+
+}
